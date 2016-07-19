43853c04a7b6f32b2a841ae19344e6840f5946e5
YanWangTF <yanxw.wang@gmail.com>
2016-07-19 10:53:18 -0400

    Update HGTfilter.py
    Added the citing information

--- HGTfilter.py
+++ HGTfilter.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python
 
+#if you would like to use or modify this script, please kindly cite the related publication:
+#Wang Y, White MM, Kvist S, Moncalvo JM. 2016. Genome-wide survey of gut fungi (Harpellales) reveals the first horizontally 
+#transferred ubiquitin gene from a mosquito host. Mol. Biol. Evol. doi:10.1093/molbev/msw126.
+
 import os
 import sys
 import numpy
